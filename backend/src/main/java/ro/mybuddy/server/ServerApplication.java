87f01c48f67505ad88025a132d9375bb89436761
Manu Dragomir <dragomir.manualex@gmail.com>
2020-11-27 12:21:11 +0200

    add swagger doc

--- backend/src/main/java/ro/mybuddy/server/ServerApplication.java
+++ backend/src/main/java/ro/mybuddy/server/ServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {"ro.mybuddy.server"})
 @EnableJpaRepositories("ro.mybuddy.server")
 @EntityScan("ro.mybuddy.server")
+@EnableSwagger2
 public class ServerApplication {
 
     public static void main(String[] args) {
